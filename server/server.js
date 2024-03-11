71fc7bdbc1ec3d6f0b3bb861c238fb009c9b37a7
Daniel Nam <danielnam@Daniels-MacBook-Pro.local>
2024-03-10 18:49:51 -0700

    successfully connected too CoinMarketCaps API, created backend endpoint that fetches cryptodata, and tested it in the frontend using BTC as an example

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-app.get("/api", (req, res) => {
-    res.json({"users" : ["userOne", "userTwo", "userThree"] })
-})
+dotenv.config();
 
-app.listen(8000, () => {console.log("Server started on port 8000")})
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.get('/:crypto', (req, res) => {
+    const crypto = req.params.crypto;
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`;
+
+    axios.get(url, {
+        headers: {
+            'X-CMC_PRO_API_KEY': process.env.API_KEY
+        }
+    })
+    .then(response => {
+        const data = response.data;
+        res.json({ data });
+    })
+    .catch(error => {
+        console.error('Error fetching cryptocurrency info:', error.message);
+        res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server listening on port 8000');
+});
